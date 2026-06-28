8a16372681511502cf4223edc8fa185f399908fa
salus <julian.guagnino@gmail.com>
2026-06-28 13:03:02 -0300

    mejores errores CI

--- correr_pruebas.sh
+++ correr_pruebas.sh
@@ -10,6 +10,8 @@ mkdir -p "$salida"
 rcfile="chastack_bdd/pruebas/.coveragerc"
 modulo_base="chastack_bdd.pruebas.__main__"
 
+EXIT_CODE=0
+
 if [ "$#" -gt 0 ]; then
     targets=()
     for arg in "$@"; do
@@ -19,9 +21,9 @@ if [ "$#" -gt 0 ]; then
             targets+=("$arg")
         fi
     done
-    coverage run --omit="*/pruebas/*" --rcfile="$rcfile" -m unittest "${targets[@]}"
+    coverage run --omit="*/pruebas/*" --rcfile="$rcfile" -m unittest "${targets[@]}" || EXIT_CODE=$?
 else
-    coverage run --omit="*/pruebas/*" --rcfile="$rcfile" -m chastack_bdd.pruebas
+    coverage run --omit="*/pruebas/*" --rcfile="$rcfile" -m chastack_bdd.pruebas || EXIT_CODE=$?
 fi
 
 coverage report --rcfile="$rcfile" | tee "$salida/informe.txt"
@@ -31,3 +33,5 @@ coverage xml --rcfile="$rcfile"
 echo
 echo "Reporte guardado en $salida"
 cd ..
+
+exit $EXIT_CODE
